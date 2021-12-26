f649a2bc9223291f41bd5692727886247c83541b
Ian Elliott <ie2173@columbia.edu>
2021-12-26 13:19:58 -0700

    created module

---
+++ address.js
@@ -0,0 +1,9 @@
+var ethers = require('ethers')
+var crypto = require('crypto')
+
+var id = crypto.randomBytes(32).toString('hex')
+var privateKey = '0x'+id
+console.log(privateKey)
+
+var wallet = new ethers.Wallet(privateKey)
+console.log("address: " + wallet.address) 
